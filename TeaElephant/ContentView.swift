a7f8d5f053b916684435d3171dec998b381c69c4
lueurxax <lueurxax@gmail.com>
2020-07-16 18:50:58 +0300

    Initial Commit

---
+++ TeaElephant/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  TeaElephant
+//
+//  Created by Andrew Khasanov on 16.07.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TeaElephant/TeaElephantApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeaElephantApp.swift
+//  TeaElephant
+//
+//  Created by Andrew Khasanov on 16.07.2020.
+//
+
+import SwiftUI
+
+@main
+struct TeaElephantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
